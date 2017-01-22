a5772706c8d7fdba02bbba170bb3218a74cf60d4
Nidhi Makhijani <nidhimj22@gmail.com>
2017-01-22 08:42:44 -0500

    url

--- app/src/main/java/com/mindorks/test/AppConfig.java
+++ app/src/main/java/com/mindorks/test/AppConfig.java
@@ -6,13 +6,15 @@ package com.mindorks.test;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
+    public static String server_ip =  "192.168.0.102";
+    public static String URL_LOGIN = "http://" + server_ip + "android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://10.0.2.2:3000//api/userLogin";
-    public static String URL_EVENT = "http://10.0.2.2:3000//api/event";
-    public static String URL_EVENTREGISTER = "http://10.0.2.2:3000//api/event/register";
-    public static String URL_GET = "http://10.0.2.2:3000//api/userLogin/27";
-    public static String URL_GETCARD = "http://10.0.2.2:3000//api/event/register/";
-    public static String URL_GETADD = "http://10.0.2.2:3000//api/event/add?";
+    public static String URL_REGISTER = "http://" + server_ip + "/10.0.2.2:3000//api/userLogin";
+    public static String URL_EVENT = "http://" + server_ip + "/api/event";
+    public static String URL_EVENTREGISTER = "http://" + server_ip + "/api/event/register";
+    public static String URL_GET = "http://" + server_ip + "/api/userLogin/27";
+    public static String URL_GETCARD = "http://" + server_ip + "/api/event/register/";
+    public static String URL_GETADD = "http://" + server_ip + "/api/event/add?";
+    public static String URL_MATCH = "http://" + server_ip + "api/event/match";
 }
